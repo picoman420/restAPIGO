bee294e3ef934a9c2a8e9babc36bb356510c65a2
Manuj Singh <manugoku.singh@gmail.com>
2024-02-03 02:37:25 -0500

    Added GET Request Function

--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
-type todo struct{
-	id 		string 	`json: "id"`
-	item 	string		`json: "item"`
-	completed 	bool	`json: "completed"`
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type todo struct {
+	ID        string `json: "id"`
+	Item      string `json: "item"`
+	Completed bool   `json: "completed"`
 }
 
 var todos = []todo{
-	{id:"1", item:"Clean Room", completed:false}
-	{id:"2", item:"Read Book", completed:false}
-	{id:"3", item:"Record Video", completed:false}
+	{ID: "1", Item: "Clean Room", Completed: false},
+	{ID: "2", Item: "Read Book", Completed: false},
+	{ID: "3", Item: "Record Video", Completed: false},
+}
+
+func getTodos(context *gin.Context) {
+
+	context.IndentedJSON(http.StatusOK, todos)
+}
+
+func main() {
+	router := gin.Default() //to create the server
+	router.GET("/todos", getTodos)
+	router.Run("localhost:9090") //to run the server on port 9090
+
 }
